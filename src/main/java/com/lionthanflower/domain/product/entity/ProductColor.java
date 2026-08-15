4c9ece6001da39035530bae1333db4d3795a7397
kimhoeyun <clnvrd@suwon.ac.kr>
2026-08-15 23:20:32 +0900

    9 feat: 제품 카탈로그 선택값 추가

---
+++ src/main/java/com/lionthanflower/domain/product/entity/ProductColor.java
@@ -0,0 +1,18 @@
+// IA에서 선택 가능한 제품 컬러를 정의하는 enum
+package com.lionthanflower.domain.product.entity;
+
+public enum ProductColor {
+  BLACK,
+  WHITE,
+  BEIGE,
+  BROWN,
+  GRAY,
+  RED,
+  PINK,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  PURPLE,
+  MULTI_COLOR
+}
---
+++ src/main/java/com/lionthanflower/domain/product/entity/ProductOption.java
@@ -0,0 +1,10 @@
+// IA에서 선택 가능한 제품 사이즈 옵션을 정의하는 enum
+package com.lionthanflower.domain.product.entity;
+
+public enum ProductOption {
+  XS,
+  S,
+  M,
+  L,
+  ONE_SIZE
+}
