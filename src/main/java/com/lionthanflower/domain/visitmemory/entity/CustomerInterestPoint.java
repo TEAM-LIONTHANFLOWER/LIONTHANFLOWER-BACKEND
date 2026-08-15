56c1056d1a58df8ae0c0bb708d87d0f509f18c49
kimhoeyun <clnvrd@suwon.ac.kr>
2026-08-15 23:21:17 +0900

    9 feat: Visit Memory 입력 선택값 추가

---
+++ src/main/java/com/lionthanflower/domain/visitmemory/entity/CustomerInterestPoint.java
@@ -0,0 +1,17 @@
+// Visit Memory에서 선택 가능한 고객 관심 포인트를 정의하는 enum
+package com.lionthanflower.domain.visitmemory.entity;
+
+public enum CustomerInterestPoint {
+  DESIGN,
+  COLOR,
+  MATERIAL_QUALITY,
+  PRACTICALITY,
+  STORAGE_FUNCTIONALITY,
+  SIZE,
+  BRAND_IDENTITY,
+  PRICE,
+  RARITY_LIMITED,
+  GIFT_SUITABILITY,
+  TRAVEL_DAILY_USE,
+  OTHER
+}
---
+++ src/main/java/com/lionthanflower/domain/visitmemory/entity/NoPurchaseReason.java
@@ -0,0 +1,18 @@
+// Visit Memory에서 선택 가능한 미구매 사유를 정의하는 enum
+package com.lionthanflower.domain.visitmemory.entity;
+
+public enum NoPurchaseReason {
+  CONSIDERING_PURCHASE,
+  NEED_COMPARE,
+  NEED_MORE_TIME,
+  CONSIDER_PRICE,
+  DESIRED_PRODUCT_OR_COLOR_NOT_FOUND,
+  TRAVEL_SCHEDULE,
+  INSUFFICIENT_TIME,
+  CONSULT_COMPANION,
+  CONSULT_GIFT_RECIPIENT,
+  BUDGET,
+  PLAN_REVISIT,
+  BROWSING_ONLY,
+  OTHER
+}
