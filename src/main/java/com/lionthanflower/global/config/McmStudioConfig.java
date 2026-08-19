99b116319564f692ed880d1f9e87dcc4e678740d
kimhoeyun <clnvrd@suwon.ac.kr>
2026-08-19 19:00:43 +0900

    40 remove: 프레임 절대 URL 설정 제거

--- src/main/java/com/lionthanflower/global/config/McmStudioConfig.java
+++
@@ -1,9 +0,0 @@
-// MCM Studio 프레임 설정 객체를 Spring Bean으로 등록하는 구성
-package com.lionthanflower.global.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties(StudioFrameProperties.class)
-public class McmStudioConfig {}
