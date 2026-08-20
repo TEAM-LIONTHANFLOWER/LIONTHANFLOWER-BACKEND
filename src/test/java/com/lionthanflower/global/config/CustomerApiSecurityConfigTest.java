63f947ed335873da22789b86e09f398660820683
kimhoeyun <clnvrd@suwon.ac.kr>
2026-08-20 21:10:57 +0900

    52 feat: API 도메인 CORS 허용 목록 추가

--- src/test/java/com/lionthanflower/global/config/CustomerApiSecurityConfigTest.java
+++ src/test/java/com/lionthanflower/global/config/CustomerApiSecurityConfigTest.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 })
 @TestPropertySource(
     properties =
-        "app.cors.allowed-origins=http://localhost:8081,https://develop.mcm-orbit-n34.pages.dev,https://mcm-orbit-n34.pages.dev,https://mcm-orbit.site")
+        "app.cors.allowed-origins=http://localhost:8081,https://develop.mcm-orbit-n34.pages.dev,https://mcm-orbit-n34.pages.dev,https://mcm-orbit.site,https://api.mcm-orbit.site")
 class CustomerApiSecurityConfigTest {
 
   @Autowired private MockMvc mockMvc;
@@ -59,7 +59,8 @@ class CustomerApiSecurityConfigTest {
         "http://localhost:8081",
         "https://develop.mcm-orbit-n34.pages.dev",
         "https://mcm-orbit-n34.pages.dev",
-        "https://mcm-orbit.site"
+        "https://mcm-orbit.site",
+        "https://api.mcm-orbit.site"
       })
   void 허용된_Origin의_API_응답에_CORS_헤더를_추가한다(String origin) throws Exception {
     mockMvc
